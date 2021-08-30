a0dc3bb6e79bd0a90470b8b7e64e904c1aa01405
Nathan Yee <nyee@csu.fullerton.edu>
2021-08-29 21:55:17 -0700

    created test js file for refactoring

---
+++ gameTest.js
@@ -0,0 +1,94 @@
+// button colors on the document
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+// game pattern created with the nextSequence function
+let gamePattern = [];
+
+
+// game level 
+let level = 0;
+
+
+// get a random sequence and push it to the gamePattern variable 
+function nextSequence() {
+        
+    // user clicked pattern
+    let userClickedPattern = [];
+        
+    // random number from 0 - 3
+    let randomNumber = (Math.random() * 4);
+    randomNumber = Math.floor(randomNumber);
+
+    // choose color from array buttonColors and push to gamePattern array
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    // fade button in and out for flash effect 
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    
+    // play audio of the random chosen color 
+    playSound(randomChosenColor);
+
+    $(document).on("click", (event) => {
+        let clickedColor = event.target.id;
+        
+        userClickedPattern.push(clickedColor);
+        console.log(userClickedPattern);
+        playSound(clickedColor);
+        animatePress(clickedColor);
+    });
+
+    level++;
+}
+
+function playSound(name) {
+    let chosenColorSound = new Audio('/sounds/' + name + '.mp3');
+    chosenColorSound.play();
+}
+
+function animatePress(currentColor) {
+    currentColor = "#" + currentColor;
+    $(currentColor).addClass("pressed");
+    setTimeout(() =>{
+        $(currentColor).removeClass("pressed");
+    }, 100);
+}
+
+// detect when a keyboard key is pressed
+let firstPress = false;
+
+$(document).on("keydown", (event) => {
+    if(firstPress === false) {
+        nextSequence();
+        firstPress = true;
+        $("#level-title").text("Level " + level);
+    }
+});
+
+function checkAnswer(index) {
+    if(userClickedPattern[index] === gamePattern[index]) {
+        console.log("user has clicked the right pattern");
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("user has clicked the wrong pattern");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Gamve over, press any key to restart");
+        $(document).on("keydown", () => {
+            startOver;
+        });
+    }
+}
+
+function startOver() { 
+    level = 0;
+    gamePattern = [];
+    firstPress = false;
+}
